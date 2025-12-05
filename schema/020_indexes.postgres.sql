974c9e663d01facb948dd0a8ea332c9c20bff17c
Vito416 <vito416@protonmail.com>
2025-12-05 16:32:16 +0100

    Regenerate PHP from schema

--- schema/020_indexes.postgres.sql
+++ schema/020_indexes.postgres.sql
@@ -2,7 +2,7 @@
 -- engine: postgres
 -- table:  authors
 
-CREATE UNIQUE INDEX IF NOT EXISTS ux_authors_tenant_slug_live_ci ON authors (tenant_id, slug_ci) WHERE deleted_at IS NULL;
+CREATE UNIQUE INDEX IF NOT EXISTS ux_authors_tenant_slug_live_ci ON authors (tenant_id, slug_ci, is_live);
 
 CREATE UNIQUE INDEX IF NOT EXISTS ux_authors_tenant_id ON authors (tenant_id, id);
 
